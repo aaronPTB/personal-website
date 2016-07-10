ff96b59895279f71b59479379742fc78fbf5a5d9
AaronPTB <aaron.ptb@gmail.com>
2016-07-10 14:29:35 -0500

    updates

---
+++ server.js
@@ -0,0 +1,23 @@
+express = require("express");
+app = express();
+
+app.use(express.static(__dirname + "/static"));
+app.use(express.static(__dirname + "/static/spoopy"));
+
+app.get("/", function(req, res) {
+	res.sendFile("index.html");
+});
+
+// commented out, only used for spoopy purposes
+// app.get("/nsfw", function(req, res) {
+// 	res.sendFile("static/spoopy/trash.html",{root: __dirname});
+// })
+
+app.listen(3000, function(err){
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("The server is running on port 300");
+    }
+});
