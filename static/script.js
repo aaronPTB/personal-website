5594ef234de8644c370a66564ef5a277b21c97d4
aaronptb <aaron.ptb@gmail.com>
2016-07-28 21:16:50 -0500

    update

--- static/script.js
+++ static/script.js
@@ -1,12 +1,15 @@
 window.onload = function() {
 	//What panel the window is currently over
 	var current_state = 0;
+	var window_positions  = [0.98, 1.96, 2.94, 3.74]
 
 	//Find the rendered width of a specific loc element
 	function getLocElementWidth(index) {
-		var loc_element = document.getElementById("loc-" + index);
-		elm_width = getComputedStyle(loc_element).width;
-		return loc_element ? elm_width : "0px";
+		if (index < window_positions.length) {
+			var loc_element = document.getElementById("loc-" + index);
+			elm_width = getComputedStyle(loc_element).width;
+			return loc_element ? elm_width : "0px";
+		}
 	}
 
 	//Binding a button to a panel will cause the screen to
@@ -14,8 +17,26 @@ window.onload = function() {
 	function bindButtonToPanel(element_id, panel_number) {
 		var element = document.getElementById(element_id);
 		element.onclick = function() {
-			window.scrollTo(0,window.innerHeight*panel_number * .98);
+			window.scrollTo(0,window.innerHeight*window_positions[panel_number]);
 		}
+	}
+
+	//Finds what state the top navigation panel should be in
+	//By comparing scroll pos to window_positions
+	function windowState(position) {
+		loc = position/window.innerHeight + .2;
+		state = 0;
+		for (var i = 0; i < window_positions.length; i++) {
+			console.log(window_positions[i])
+			if (loc > window_positions[i]) {
+				state = state + 1;
+			}
+			else {
+				break;
+			}
+		}
+		console.log(state)
+		return state;
 	}
 
 	bindButtonToPanel("loc-0", 0);
@@ -27,14 +48,12 @@ window.onload = function() {
 	window.onscroll = function() {
 		var pos = window.pageYOffset;
 
-		//Figure out current panel based on scroll pos
-		state = Math.floor(pos/(.98 * window.innerHeight) + .2);
-
+		state = windowState(pos);
 		if (state != current_state) {
 			var underline = document.getElementById("underline");
-			
-			underline.style.left  = [16,95,183,275][state];
-			underline.style.width = getLocElementWidth(state); 
+
+			underline.style.left  = [13,92,180,270][state];
+			underline.style.width = getLocElementWidth(state);
 
 			current_state = state;
 		}
