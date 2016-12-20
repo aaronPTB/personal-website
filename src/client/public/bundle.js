d906b2b491dec0542ac1c5bae0dad2a21c8276cd
Aaron Evans <aaron.ptb@gmail.com>
2016-12-20 17:52:06 -0600

    update

--- src/client/public/bundle.js
+++ src/client/public/bundle.js
@@ -26926,7 +26926,7 @@
 	            null,
 	            "Hey! I'm a 19 year old Computer Science ",
 	            _react2.default.createElement("br", null),
-	            " major at the University of Texas at Austin. My current interests lie in web development, operating systems, robotics, and machine learning."
+	            " major at the Universityof Texas at Austin and incoming summer intern at Audible, an Amazon Company. My current interests lie in web development, operating systems, robotics, and machine learning."
 	          )
 	        )
 	      );
