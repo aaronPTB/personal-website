5bb64e5f45b8cc594f323b2ed7b49ff45480a7de
Aaron Evans <aaron.ptb@gmail.com>
2016-12-20 21:35:33 -0600

    update

--- src/client/app/home.jsx
+++ src/client/app/home.jsx
@@ -19,13 +19,13 @@ export default class Home extends React.Component {
         </div>
         <div id="about-me" className="column-1000">
           <p className="about-text">
-            Hey! I'm a 19 year old Computer Science <br/> major at the
+            Hey! I'm a 19 year old Computer Science major at the
             University of Texas at Austin and incoming summer intern at Audible,
             an Amazon Company. Currently interested in web development,
             operating systems, robotics, and machine learning.
             <br/><br/>
             Need to contact me? Email me at aaron [at] cs.utexas.edu, or just
-            catch me on social media.
+            catch me on social media."}
           </p>
           <div className="social-media-panel">
             <i className="fa fa-twitter"></i>
--- src/client/public/bundle.js
+++ src/client/public/bundle.js
@@ -26924,12 +26924,10 @@
 	          _react2.default.createElement(
 	            "p",
 	            { className: "about-text" },
-	            "Hey! I'm a 19 year old Computer Science ",
-	            _react2.default.createElement("br", null),
-	            " major at the University of Texas at Austin and incoming summer intern at Audible, an Amazon Company. Currently interested in web development, operating systems, robotics, and machine learning.",
+	            "Hey! I'm a 19 year old Computer Science major at the University of Texas at Austin and incoming summer intern at Audible, an Amazon Company. Currently interested in web development, operating systems, robotics, and machine learning.",
 	            _react2.default.createElement("br", null),
 	            _react2.default.createElement("br", null),
-	            "Need to contact me? Email me at aaron [at] cs.utexas.edu, or just catch me on social media."
+	            "Need to contact me? Email me at aaron [at] cs.utexas.edu, or just catch me on social media.\"}"
 	          ),
 	          _react2.default.createElement(
 	            "div",
